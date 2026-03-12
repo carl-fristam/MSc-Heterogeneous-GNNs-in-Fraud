cb5f54cf2fa34fbb1fd562bf63127bdd06774de6
carlF <carl.fristam@gmail.com>
2026-03-12 09:58:48 +0100

    Cleanup

--- src/baselines/graphsage/data.py
+++
@@ -1,4 +0,0 @@
-from src.data.saml_homo import load_and_prepare_saml_data
-
-def load_and_prepare_data(sample_Ratio = 1.0, use_cache = True):
-    return load_and_prepare_saml_data(sample_ratio=sample_Ratio, use_cache=use_cache)
